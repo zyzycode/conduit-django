758355a2c523fe2b652ea2c94c88f18d751a7588
zybzick <arclanov94@gmail.com>
2020-06-26 19:25:07 +0500

    Creating the Profile model

---
+++ conduit/apps/profiles/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        'authentication.User', on_delete=models.CASCADE
+    )
+    bio = models.TextField(blank=True)
+    image = models.URLField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
